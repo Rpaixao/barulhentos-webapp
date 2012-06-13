0bd07a48cf89d12599a4945f87c34a758a854ba0
rubenpaixao <rubenpaixao@rubenair-pc.lan>
2012-06-13 23:55:09 +0100

    -Primeira versão de demonstração dos Sócios criada.

--- barulhentos-webapp/src/java/partnerManager.java
+++ barulhentos-webapp/src/java/partnerManager.java
@@ -1,6 +1,12 @@
 
+import barulhentos.javaapi.Partner;
 import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
+import services.ServiceManager;
 
 /*
  * To change this template, choose Tools | Templates
@@ -11,11 +17,40 @@ import javax.faces.bean.ManagedBean;
  *
  * @author rubenpaixao
  */
-@ManagedBean(name = "userManager")
+@ManagedBean(name = "partnerManager")
 public class partnerManager implements Serializable{
     
+    LinkedList<Partner> partners ;
+
+    public LinkedList<Partner> getPartners() {
+        return partners;
+    }
+    ServiceManager manager;
+    
+    public partnerManager(){
+        try {
+            System.out.println("Entrei no partnerManager");
+            this.partners = new LinkedList<Partner>();
+            manager = new ServiceManager();
+            partners = manager.userService().getSocios();
+            
+            for(Partner p : partners){
+                System.out.println(p.getName());
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(partnerManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(partnerManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(partnerManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(partnerManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
     public String load(){
         return "success";
     }
+
 }
